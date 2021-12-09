025d23896a523b7f993629c8356d53b4099255b3
Emily Burger <eburger939@gmail.com>
2021-12-09 15:41:35 -0600

    changed to four 4 loops instead of one

--- assets/JS/script2.js
+++ assets/JS/script2.js
@@ -3,18 +3,15 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-// var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var result = [];
-// var finalResult = result.join('');
-// var end = '';
+
 
 
 
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(_event) {
-    event.preventDefault();
+function generatePassword() {
     var passwordLength = window.prompt("How long do you want your password? (must be between 8-128)");
     if (!passwordLength || passwordLength < 8 || passwordLength >= 128) {
         window.alert("this is an invalid response");
@@ -23,51 +20,45 @@ function generatePassword(_event) {
 
     var userLowercase = window.confirm("Do you want lowercase letters?");
     if (userLowercase === true) {
-          result += lowercase; 
-    // } else if (userLowercase === false) {
-      // result;
+        for (var i = 0; i <passwordLength; i++)
+        result[i] = [Math.floor(Math.random() * lowercase.length)]
+        console.log(result);
     }
 
     var userUppercase = window.confirm("Do you want uppercase letters?");
     if (userUppercase === true) {
-      result += uppercase; 
-    // } else if (userUppercase === false) {
-      // result;
+        for (var i = 0; i <passwordLength; i++)
+        result[i] = [Math.floor(Math.random() * uppercase.length)]
+        console.log(result);
     }
 
     var userNumeric = window.confirm("Do you want numbers?");
     if (userNumeric === true) {
-      result += numeric;
-    // } else if (userNumeric === false) {
-      // result;
+        for (var i = 0; i <passwordLength; i++)
+        result[i] = [Math.floor(Math.random() * numeric.length)]
+        console.log(result)
     }
 
     var userSpecial = window.confirm("Do you want special characters?");
     if (userSpecial === true) {
-      result += special;
-    // } else if (userSpecial === false) {
-      // result;
-      //  return result.join(""); 
-}
- for (var i = 0; i < passwordLength; i++){
-    result[i] = [Math.floor(Math.random() * result.length)];
-    console.log(result[i])
-    // passwordText.value = result. join();
-  }
+        for (var i = 0; i <passwordLength; i++)
+        result[i] = [Math.floor(Math.random() * special.length)]
+        console.log(result);
+    }
+    // return result.join('');
 
 }
-
-  
 
 // Write password to the #password input
-function writePassword(event) {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  event.preventDefault();
+  
+  
   passwordText.value = password; 
-  //does this need to include prevent default 
+
+
+  }
  
- 
-}
 
 
