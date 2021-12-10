a28f8cd1af8396955b43dd9bb373fb88f18e0fea
Emily Burger <eburger939@gmail.com>
2021-12-09 19:02:28 -0600

    finished js, working on readme, need screenshot)

--- assets/JS/script2.js
+++ assets/JS/script2.js
@@ -5,6 +5,7 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var result = [];
+var possiableArray = [];
 
 
 
@@ -20,32 +21,51 @@ function generatePassword() {
 
     var userLowercase = window.confirm("Do you want lowercase letters?");
     if (userLowercase === true) {
-        for (var i = 0; i <passwordLength; i++)
-        result[i] = [Math.floor(Math.random() * lowercase.length)]
-        console.log(result);
+        for (var i = 0; i <passwordLength; i++){
+        var possiableIndex = Math.floor(Math.random() * lowercase.length);
+        var possiableChar = lowercase[possiableIndex];
+        possiableArray.push(possiableChar);
+    
+        }
     }
 
     var userUppercase = window.confirm("Do you want uppercase letters?");
     if (userUppercase === true) {
-        for (var i = 0; i <passwordLength; i++)
-        result[i] = [Math.floor(Math.random() * uppercase.length)]
-        console.log(result);
+        for (var i = 0; i <passwordLength; i++){
+        var possiableIndex = Math.floor(Math.random() * uppercase.length);
+        var possiableChar = uppercase[possiableIndex];
+        possiableArray.push(possiableChar);
+       
+    }
     }
 
     var userNumeric = window.confirm("Do you want numbers?");
     if (userNumeric === true) {
-        for (var i = 0; i <passwordLength; i++)
-        result[i] = [Math.floor(Math.random() * numeric.length)]
-        console.log(result)
+        for (var i = 0; i <passwordLength; i++) {
+        var possiableIndex = Math.floor(Math.random() * numeric.length);
+        var possiableChar = numeric[possiableIndex];
+        possiableArray.push(possiableChar);
+        } 
     }
 
     var userSpecial = window.confirm("Do you want special characters?");
     if (userSpecial === true) {
-        for (var i = 0; i <passwordLength; i++)
-        result[i] = [Math.floor(Math.random() * special.length)]
-        console.log(result);
+        for (var i = 0; i <passwordLength; i++) {
+        var possiableIndex = Math.floor(Math.random() * special.length);
+        var possiableChar = special[possiableIndex];
+        possiableArray.push(possiableChar);
+        }
     }
-    // return result.join('');
+
+    console.log(possiableArray);
+
+    for (var i = 0; i<passwordLength; i++) {
+        var possiableIndex = Math.floor(Math.random() * possiableArray.length);
+        var possiableChar = possiableArray[possiableIndex];
+        result.push(possiableChar);
+    }
+
+    return result.join('');
 
 }
 
